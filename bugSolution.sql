30226410255f90ba52cdb2fa1a36bc7f853b06bb
BugHunter-X <askzenai@gmail.com>
2024-12-22 22:40:05 +0100

    Add bugSolution.sql file

---
+++ bugSolution.sql
@@ -0,0 +1,5 @@
+```sql
+SELECT * FROM employees WHERE department = 'Sales' AND salary >= 100000;
+```
+
+This corrected query uses the '>=' operator to include employees with salaries greater than or equal to 100000, thus fixing the exclusion error.
